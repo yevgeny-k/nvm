7eff09a64188a78ac4cc14d0b2ab02f3230f9ec3
Evgeniy Kozin <evgeniy.kozin@gmail.com>
2012-04-17 16:29:07 +0400

    Makefile для flash-плеера

--- flashplayer/build_step1.cmd
+++
@@ -1,7 +0,0 @@
-SET INFILE=%CD%\src\kprftvlivec.as
-SET OUTFILE=%CD%\bin\kprftvlivec.swf
-
-chcp 1251
-cd /flex/bin/
-mxmlc.exe -static-link-runtime-shared-libraries=true -metadata.creator=EvgeniyKozin %INFILE% -output=%OUTFILE%
-pause
